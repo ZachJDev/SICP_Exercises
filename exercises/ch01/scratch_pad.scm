6edcd7f4a9d8641bc445e0f1c740f84da47b9874
ZachJDev <ZachJDev2121@gmail.com>
2023-10-28 15:35:49 -0400

    add scract pad

---
+++ exercises/ch01/scratch_pad.scm
@@ -0,0 +1,31 @@
+#lang sicp
+(define (square x) (* x x))
+(define (average x y) (/ (+ x y) 2))
+(define (sqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
+
+; (sqrt 25)
+
+(define (factorial n)
+  (if (<= n 1)
+      n
+      (* n (factorial (- n 1)))))
+
+(factorial 6)
+
+(define (factorial-iter n)
+  (define (fact-iter acc current)
+    (if (> current n)
+        acc
+        (fact-iter (* acc current)(+ 1 current))))
+  (fact-iter 1 1))
+
+(factorial-iter 6)
