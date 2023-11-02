6a05a9fe0b48d215aa70ec09db6b7e344abae29b
ZachJDev <ZachJDev2121@gmail.com>
2023-11-01 21:54:21 -0400

    check in

---
+++ exercises/ch01/1.3.rkt
@@ -0,0 +1,19 @@
+#lang sicp
+(define (sum func from to)
+  (if (> from to)
+      0
+      (+ (func from) (sum func (+ 1 from) to))))
+
+(define (square x) (* x x))
+
+(sum (lambda (x) (* x x)) 1 3)
+
+(define (sum2 from to)
+  (lambda (func)
+      (if (> from to)
+      0
+      (+ (func from) (sum func (+ 1 from) to)))))
+
+((sum2 1 3) square)
+((sum2 1 ((sum2 1 3) square)) (lambda (x) x))
+(sum (lambda (x) x) 1 14)
