e543f1b66116f7183e88db027f4258d04067a6b7
ZachjDev <ZachJDev2121@gmail.com>
2023-12-06 06:05:32 -0500

    exercise 1.16

---
+++ exercises/ch01/1.2.rkt
@@ -0,0 +1,15 @@
+#lang sicp
+
+; EXERCISE 1.16
+
+(define (square x) (* x x))
+
+(define (exp-iter x exp)
+(define (iter b n a)
+  (cond ((= n 0) a)
+        ((even? n) (iter (square b) (/ n 2) a))
+        (else (iter b (- n 1) (* a b)))))
+ (iter x exp 1))
+
+(exp-iter 3 5)
+        
