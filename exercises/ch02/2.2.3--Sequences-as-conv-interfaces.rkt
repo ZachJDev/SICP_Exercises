7c52cde5a62c8559479bef78fdd612ba7054d971
ZachjDev <ZachJDev2121@gmail.com>
2024-01-08 06:16:04 -0500

    a few exercises

---
+++ exercises/ch02/2.2.3--Sequences-as-conv-interfaces.rkt
@@ -0,0 +1,47 @@
+#lang sicp
+
+(define (square x) (* x x))
+
+(define (filter predicate seq)
+    (cond ((null? seq) '())
+          ((predicate (car seq)) (cons (car seq) (filter predicate (cdr seq))))
+          (else 
+            (filter predicate (cdr seq)))))
+
+(define (accumulate op initial seq)
+    (if (null? seq)
+        initial
+        (op (car seq) (accumulate op initial (cdr seq)))
+        ))
+
+(define (enumerate-interval low high)
+    (if (> low high) '()
+     (cons low (enumerate-interval (+ 1 low) high))))
+
+(define (enumerate-tree tree)
+    (cond ((null? tree) '())
+          ((not (pair? tree)) (list tree))
+          (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+
+(define (sum-odd-squares tree)
+   (accumulate + 0
+    (map square (filter odd? (enumerate-tree tree)))))
+
+; EXERCISE 2.36
+(define (accumulate-n op init seqs)
+    (if (null? (car seqs))
+        '()
+        (cons (accumulate op init (map car seqs))
+              (accumulate-n op init (map cdr seqs)))))
+
+; EXERCISE 2.37
+(define (dot-product v w)
+    (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+    (map (lambda (mi) (dot-product v mi)) m))
+
+(define (transpose mat)
+    (accumulate-n cons '() mat))
